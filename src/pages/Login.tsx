66423abd659027afa0305fdddc1183a9488344a3
JamesSerenio <sereniojames363@gmail.com>
2025-12-30 18:06:39 +0800

    added login and design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,8 +5,12 @@ import {
   IonInput,
   IonButton,
   IonItem,
-  IonLabel,
+  IonIcon,
 } from "@ionic/react";
+import { mailOutline, lockClosedOutline } from "ionicons/icons";
+
+import studyHubLogo from "../assets/study_hub.png";
+import leaves from "../assets/leave.png";
 
 const Login: React.FC = () => {
   const [emailFocused, setEmailFocused] = useState(false);
@@ -15,18 +19,35 @@ const Login: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen className="login-content">
+
+        {/* CORNER LEAVES */}
+        <img src={leaves} className="leaf leaf-top-left" alt="leaf" />
+        <img src={leaves} className="leaf leaf-top-right" alt="leaf" />
+        <img src={leaves} className="leaf leaf-bottom-left" alt="leaf" />
+        <img src={leaves} className="leaf leaf-bottom-right" alt="leaf" />
+
         <div className="login-wrapper">
           <div className="login-box">
-            <h2>Login</h2>
+
+            {/* TITLE + LOGO */}
+            <div className="login-header">
+              <img
+                src={studyHubLogo}
+                alt="Study Hub Logo"
+                className="login-logo"
+              />
+              <h2>Login</h2>
+            </div>
 
             {/* EMAIL */}
             <IonItem
               lines="none"
               className={`input-item ${emailFocused ? "item-has-focus" : ""}`}
             >
-              <IonLabel position="floating">Email</IonLabel>
+              <IonIcon icon={mailOutline} className="input-icon" />
               <IonInput
                 type="email"
+                placeholder="Enter email"
                 onIonFocus={() => setEmailFocused(true)}
                 onIonBlur={() => setEmailFocused(false)}
               />
@@ -37,9 +58,10 @@ const Login: React.FC = () => {
               lines="none"
               className={`input-item ${passwordFocused ? "item-has-focus" : ""}`}
             >
-              <IonLabel position="floating">Password</IonLabel>
+              <IonIcon icon={lockClosedOutline} className="input-icon" />
               <IonInput
                 type="password"
+                placeholder="Enter password"
                 onIonFocus={() => setPasswordFocused(true)}
                 onIonBlur={() => setPasswordFocused(false)}
               />
@@ -48,8 +70,10 @@ const Login: React.FC = () => {
             <IonButton expand="block" className="login-btn">
               Login
             </IonButton>
+
           </div>
         </div>
+
       </IonContent>
     </IonPage>
   );
